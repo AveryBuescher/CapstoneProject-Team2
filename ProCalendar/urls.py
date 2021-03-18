a0b110805d0f1cf66f44f9bc336b18870132d270
zwawrzaszek <zwawrzaszek@my.waketech.edu>
2021-03-18 04:47:48 -0400

    Added some parts of login feature

--- ProCalendar/urls.py
+++ ProCalendar/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('cal.urls')),
+]
+
+# Creates path for login view
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
