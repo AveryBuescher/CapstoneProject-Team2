221a189efd44ed69ebb941b5a8a0f707c6f6720c
Avery Buescher <47182446+AveryBuescher@users.noreply.github.com>
2021-04-01 15:33:36 -0400

    Added initial redirect to /calendar, centered login elements, added avery superuser

--- cal/urls.py
+++ cal/urls.py
@@ -7,6 +7,7 @@ from . import views
 
 app_name = 'cal'
 urlpatterns = [
+    url(r'^$', login_required(views.CalendarView.as_view()), name='home'),
     url(r'^index/$', views.index, name='index'),
     url(r'^calendar/$', login_required(views.CalendarView.as_view()), name='calendar'),
     url(r'^event/new/$', views.event, name='event_new'),
